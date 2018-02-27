d51fa6b679068dd16678f7b2d600a525fca070a6
ismaelalvesgit <cearaismael1997@gmail.com>
2018-02-27 09:45:44 -0300

    exemplos

--- graficos/src/graficos/Game.java
+++ graficos/src/graficos/Game.java
@@ -1,18 +1,35 @@
 package graficos;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 import javax.swing.JFrame;
 
 public class Game extends Canvas implements Runnable{
 	
 	public static JFrame frame;
-	private final int WIDTH = 160;
-	private final int HEIGHT = 120;
-	private final int SCALE = 4;
+	private Thread thread;
+	private boolean isRunning = true;
+	private final int WIDTH = 240;
+	private final int HEIGHT = 160;
+	private final int SCALE = 3;
+	private BufferedImage image;
+	private Spritesheet sheet;
+	private BufferedImage player;
 	
 	public Game() {
+		sheet = new Spritesheet("/player.png");
+		player = sheet.getSprite(0, 0, 16, 16);
 		this.setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+		initframe();
+		image = new BufferedImage(160, 120,BufferedImage.TYPE_INT_RGB);
+	}
+	
+	public void initframe() {
 		frame = new JFrame("game #1");
 		frame.add(this);
 		frame.setResizable(false);
@@ -22,13 +39,81 @@ public class Game extends Canvas implements Runnable{
 		frame.setVisible(true);
 	}
 	
+	public synchronized void start() {
+		isRunning = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+	
+	public synchronized void stop() {
+		isRunning = false;
+		
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args) {
 		Game game = new Game();
+		game.start();
 	}
+	
+	public void tick() {
+		
+	}
+	
+	public void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		
+		if (bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		Graphics g = image.getGraphics();
+		g.setColor(new Color(19,19,19,19));
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		/*Renderização do jogo*/
+		g.drawImage(player, 20, 20, null);
+		
+		/**/
+		g.dispose();
+		g = bs.getDrawGraphics();
+		g.drawImage(image, 0, 0, WIDTH*SCALE, HEIGHT*SCALE, null);
+		bs.show();
+	}
+	
 	@Override
 	public void run() {
+		//logica de ciclos de render e tick
+		long lastTime = System.nanoTime();
+		double amoutOfTicks = 60.0;
+		double ns = 1000000000 / amoutOfTicks;
+		double delta = 0;
+		int frames = 0;
+		double timer = System.currentTimeMillis();
 		
 		
+		while(isRunning) {
+			
+			long now = System.nanoTime();
+			delta+= (now - lastTime) / ns;
+			lastTime = now;
+			if(delta >=1 ) {
+				tick();
+				render();
+				frames++;
+				delta --;
+			}
+			if(System.currentTimeMillis() - timer >= 1000) {
+				System.out.println("FPS:" + frames );
+				frames = 0;
+				timer+=1000;
+			}
+		}
+		stop();
 	}
 	
 }
